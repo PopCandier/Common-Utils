e05116d475490d46ac8f436533dd473a65667d99
Pop <824173124@qq.com>
2019-04-23 20:13:22 +0800

    HTML 的 Option与Select的添加与反射工具的的增加

---
+++ src/main/java/com/pop/uitils/StringUtils.java
@@ -0,0 +1,12 @@
+package com.pop.uitils;
+
+/**
+ * @author Pop
+ * @date 2019/4/23 20:09
+ */
+public class StringUtils {
+
+    public static Boolean isNotEmpty(String src){ return src!=null&&src!=""?true:false; }
+    public static Boolean isEmpty(String src){return src==null||src==""?true:false;}
+
+}
